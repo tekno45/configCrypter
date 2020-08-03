d8c97ff31c9bd2ea1df3f328bffb25522a6b58df
tekno45 <megamanX42002@gmail.com>
2020-08-02 20:02:12 -0700

    switched aws clients to interfaces for testing
    Interfaces allow mocking API for testing

--- crypt.go
+++ crypt.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -9,13 +10,16 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-
 	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
 )
 
-func decryptFile(targetFile *string, client kms.KMS) {
+//decryptFile takes KMS encrypted file requests AWS KMS to decrypt it
+func decryptFile(targetFile *string, kmsClient kmsiface.KMSAPI) {
 	text, err := ioutil.ReadFile(*targetFile)
 	if err != nil {
 		log.Fatal("can't read secret file")
@@ -24,38 +28,32 @@ func decryptFile(targetFile *string, client kms.KMS) {
 		CiphertextBlob: text,
 	}
 
-	response, err := client.Decrypt(payload)
+	response, err := kmsClient.Decrypt(payload)
 	fmt.Println("Decrypting")
 	fmt.Println(string(response.Plaintext))
 
 }
 
-//encryptFile takes in a file path and returns the KMS encrypted data
-func encryptFile(targetFile *string, kmsID *string, client kms.KMS, pipe chan<- []byte) {
-	text, err := ioutil.ReadFile(*targetFile)
-	if err != nil {
-		log.Fatal("Cannot read file: ", *targetFile, "\n", err)
-	}
-
+//encryptFile takes in a file path and puts the KMS encrypted data to a channel
+func encryptFile(buf bytes.Buffer, kmsID *string, kmsClient kmsiface.KMSAPI, pipeInput chan<- []byte) {
+	text := buf.Bytes()
 	var input kms.EncryptInput
 	input.KeyId = kmsID
 	input.Plaintext = text
-
-	output, err := client.Encrypt(&kms.EncryptInput{
+	output, err := kmsClient.Encrypt(&kms.EncryptInput{
 		KeyId:     aws.String(*kmsID),
 		Plaintext: text})
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(text))
-	fmt.Println(output.String())
-	fmt.Println(string(output.CiphertextBlob))
-	pipe <- output.CiphertextBlob
+	pipeInput <- output.CiphertextBlob
+
 }
 
 //writeEncryptedFile writes the encrypted data to disk and creates the folder to hold them
-func writeEncryptedFile(outputFolder *string, osPerms *int, path *string, pipe chan []byte) string {
-	file := <-pipe
+func writeEncryptedFile(outputFolder *string, osPerms *int, path *string, pipeInput chan []byte) string {
+	file := <-pipeInput
 	perms := os.FileMode(*osPerms)
 	filename := *outputFolder + filepath.Base(*path)
 
@@ -76,26 +74,49 @@ func main() {
 	kmsID := flag.String("kms", "", "KMS Key to use to encrypt the file")
 	region := flag.String("region", "us-west-1", "region with KMS key")
 	flag.Parse()
-	config_file, err := ioutil.ReadFile("file_list.txt")
+	configFile, err := ioutil.ReadFile("file_list.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	files := strings.Split(string(config_file), "\n")
+	files := strings.Split(string(configFile), "\n")
 	if len(files) < 1 {
 		log.Fatal("usage: ./cfgcrpyt -o=./encryptedOutPut/ /path1/file1 /path2/file2")
 	}
 	sess := session.Must(session.NewSession())
 
-	client := *kms.New(sess, aws.NewConfig().WithRegion(*region))
+	kmsClient := kms.New(sess, aws.NewConfig().WithRegion(*region))
+	//s3Client := s3.New(sess, aws.NewConfig().WithRegion(*region))
 	osPerms := int(0667)
-	pipe := make(chan []byte)
+	pipeInput := make(chan []byte)
+	//pipeOutput := make(chan []byte)
 
 	for x := range files {
-		fmt.Println("Encrypting: ", files[x])
 		file := strings.TrimSpace(files[x])
 		if file != "" {
-			go encryptFile(&file, kmsID, client, pipe)
-			fmt.Println(writeEncryptedFile(outputFolder, &osPerms, &files[x], pipe))
+			fmt.Println("Encrypting: ", files[x])
+			text, err := ioutil.ReadFile(file)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var buf bytes.Buffer
+			buf.Write(text)
+			//uri, err := url.Parse(file)
+			if err != nil {
+				go encryptFile(buf, kmsID, kmsClient, pipeInput)
+				writeEncryptedFile(outputFolder, &osPerms, &files[x], pipeInput)
+			}
+			go encryptFile(buf, kmsID, kmsClient, pipeInput)
+			writeEncryptedFile(outputFolder, &osPerms, &files[x], pipeInput)
+			// Block for checking URL scheme for s3 upload
+			//	switch uri.Scheme {
+			//	case "s3":
+			//		return
+			//	default:
+			//		return
+			//	}
+
 		}
+
 	}
 }
